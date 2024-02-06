12df33619812da0599f2bf318fe8342c1ddf6d35
Tom Spencer <thomas.spencer2034@gmail.com>
2024-02-06 16:15:34 -0500

    Add players to game

--- game.rb
+++ game.rb
@@ -1,11 +1,14 @@
 class Game
+
+# Set up game here (e.g., players, scores)
   def initialize
-player_1 = Person.new('Player 1', 3)
-player_2 = Person.new('Player 2', 3)
-    # Set up your game here (e.g., players, scores)
+    @player_1 = Person.new('Player 1', 3)
+    @player_2 = Person.new('Player 2', 3)
   end
 
   def start
+  puts @player_1.name
+  puts @player_1.lives
     # The game loop or logic to start the game
   end
 
